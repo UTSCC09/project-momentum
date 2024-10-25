1a4f8b84013e9411d02ca1b3976bdea1b80ca53b
Kaiwei Zhang <134007383+KevinZJN@users.noreply.github.com>
2024-10-25 18:14:06 -0400

    init

---
+++ backend/src/index.ts
@@ -0,0 +1,26 @@
+import express, { Express } from "express";
+import cors from "cors";
+
+import { initTRPC } from "@trpc/server";
+import { createExpressMiddleware } from "@trpc/server/adapters/express";
+
+const app: Express = express();
+
+const trpc = initTRPC.create();
+const trpcRouter = trpc.router({
+  hello: trpc.procedure.query(() => {
+    return "Hello, World!"
+  }),
+
+  logtoserver: trpc.procedure.input((v) => {
+    console.log(v);
+    return "Logged!";
+  }).mutation(req => {
+    console.log("From client"+req.input);
+  })
+});
+
+app.use("/trpc", createExpressMiddleware({router: trpcRouter}));
+
+app.use(cors({ origin: "http://localhost:3125" }));
+app.listen(3000 , () => console.log("Server running on http://localhost:3000"));

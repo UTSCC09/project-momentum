ee060d905a31405474a105f798dcfd084ca96f0d
Kaiwei Zhang <134007383+KevinZJN@users.noreply.github.com>
2024-10-26 16:53:41 -0400

    google oauth

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,19 @@
 import express, { Express } from "express";
 import cors from "cors";
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
+
+import { oauthRouter } from "./routers/google_oauth";
+
 const app: Express = express();
+app.use(cors({ origin: "http://localhost:5173" }));
 
 import { trpcRouter } from "./routers/index";
 
+app.get("/", (req, res) => {
+    res.json("Hello, World!");
+});
 
-app.use(cors({ origin: "http://localhost:3125" }));
+app.use("/api/oauth", oauthRouter);
 app.use("/trpc", createExpressMiddleware({router: trpcRouter}));
 app.listen(3000 , () => console.log("Server running on http://localhost:3000"));
 
---
+++ backend/src/routers/google_oauth.ts
@@ -0,0 +1,64 @@
+import { google } from "googleapis";
+import { Router } from "express";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const client_id = process.env.GOOGLE_CLIENT_ID;
+const client_secret = process.env.GOOGLE_CLIENT_SECRET;
+const redirect_url =
+  process.env.REDIRECT_URL || "http://localhost:3000/api/oauth/googlecallback";
+
+export const oauthRouter = Router();
+
+const oauth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_url,
+);
+
+const scopes = [
+    "https://www.googleapis.com/auth/userinfo.email",
+    "https://www.googleapis.com/auth/userinfo.profile",
+];
+
+const url = oauth2Client.generateAuthUrl({
+    access_type: "offline", // use offline to get refresh token
+    scope: scopes,
+    prompt: "consent",
+  });
+
+oauthRouter.post("/signin", (req, res) => {
+    // Signin logic here
+    res.json({ url });
+    // send a fetch to googlecallback
+});
+
+oauthRouter.get("/googlecallback", async (req, res) => {
+  const GOOGLE_ACCESS_TOKEN_URL='https://oauth2.googleapis.com/token'
+
+  const { code } = req.query;
+
+  const data = {
+    code,
+
+    client_id: client_id,
+
+    client_secret: client_secret,
+
+    redirect_uri: "http://localhost:3000/api/oauth/googlecallback",
+
+    grant_type: "authorization_code",
+  };
+
+  const response = await fetch(GOOGLE_ACCESS_TOKEN_URL, {
+    method: "POST",
+
+    body: JSON.stringify(data),
+  });
+
+  const access_token_data = await response.json();
+
+  console.log(access_token_data);
+  res.redirect('http://localhost:5173/')
+});

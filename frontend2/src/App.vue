90cbaf7855ef07bd5a5a1a5d80915a32e734e0d6
Kaiwei Zhang <134007383+KevinZJN@users.noreply.github.com>
2024-10-26 23:30:55 -0400

    a frontend vue to test oauth

---
+++ frontend2/src/App.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+
+const urlParams = new URLSearchParams(window.location.search);
+const token = urlParams.get('token');
+
+if (token){
+  console.log(token);
+}
+
+function login() {
+  fetch('http://localhost:3000/api/oauth/google/signin', {
+    method: 'POST',
+  })
+  .then(response => response.json())
+  .then(data => {window.location.href = data.url;})
+}
+
+function login2() {
+  fetch('http://localhost:3000/api/oauth/microsoft/signin', {
+    method: 'POST',
+  })
+  .then(response => response.json())
+  .then(data => {window.location.href = data.url;})
+}
+
+</script>
+
+<template>
+  <button @click=login>Click me</button>
+
+  <button @click=login2>Click me</button>
+</template>
+
+<style scoped>
+</style>

908e3582d315f78ea692d490418b5f29ccedd258
Kaiwei Zhang <134007383+KevinZJN@users.noreply.github.com>
2024-11-21 00:21:20 -0500

    update date format

--- frontend/src/api/utils.ts
+++ frontend/src/api/utils.ts
@@ -1,24 +1,16 @@
 import moment from 'moment';
-// replace evertime you see new Date() with moment()
-console.log(moment().format('YYYY-MM-DD HH:mm:ss'));
 
 // Format: YYYY-MM-DD HH:MM:SS
 export function formatDatetime(date: Date | string) {
-  if (typeof date == "string") {
-    return date.slice(0, 19).replace('T', ' ');
-  }
-  return date.toISOString().slice(0, 19).replace('T', ' ');
+  return moment(date).format('YYYY-MM-DD HH:mm:ss');
 }
 
 // Format: YYYY-MM-DD
 export function formatDate(date: Date | string) {
-  if (typeof date == "string") {
-    return new Date(date).toISOString().slice(0, 10);
-  }
-  return date.toISOString().slice(0, 10);
+  return moment(date).format('YYYY-MM-DD');
 }
 
 // Format: YYYYMMDD
 export function formatFloatDate(date: Date) {
-  return date.toISOString().slice(0, 10).replaceAll("-", "");
+  return moment(date).format('YYYYMMDD');
 }
